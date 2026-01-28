ba0b4c47e2df699413ec86b0192c25fa61b1463e
kdrolle594 <kdrolle594@outlook.com>
2026-01-28 07:51:38 -0500

    Improved security

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDqtVX02iLsYi3jKdb4_r_c9I6wshHM6io",
+    authDomain: "tetris-game-86c46.firebaseapp.com",
+    databaseURL: "https://tetris-game-86c46-default-rtdb.firebaseio.com",
+    projectId: "tetris-game-86c46",
+    storageBucket: "tetris-game-86c46.firebasestorage.app",
+    messagingSenderId: "662747534312",
+    appId: "1:662747534312:web:2b3433a5100eaa53320a5d",
+    measurementId: "G-RKPDJNHC0P"
+};
